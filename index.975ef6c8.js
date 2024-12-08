114d8a7208c716407878781153958bc1ac58dff1
Alexandr Semenets <145280327+seemyoon@users.noreply.github.com>
2024-12-08 23:55:06 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27234,7 +27234,7 @@ const imageAltText = "purple and blue abstract background";
  * Use this to give more information about what you are passionate about,
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
- */ const detailOrQuote = "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+ */ const detailOrQuote = "I'm a full-stack developer studying at Kharkiv National University of Economics. I enjoy creating unique and efficient solutions for both the frontend and backend, merging functionality with design.";
 const About = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("section", {
         className: "padding",
