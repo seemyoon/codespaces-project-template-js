353e79830dc7e3cac26eac36f2a365f31e2ad14a
Alexandr Semenets <145280327+seemyoon@users.noreply.github.com>
2024-12-08 23:47:09 +0000

    created portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/mosaic.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -31,12 +31,29 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+ 'Node JS', 
+'TypeScript', 
+'JavaScript', 
+'React', 
+'NestJs', 
+'Express', 
+'Redux', 
+'Next JS', 
+'MongoDB', 
+'MySQL', 
+'Postgres', 
+'Docker', 
+'Kubernetes', 
+'AWS', 
+'Npm', 
+'Redis', 
+'Linux',  
+'HTML 5', 
+'CSS 3', 
+'SASS', 
+'Git', 
+'GitHub', 
+'Postman',  
 ];
 
 /**
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -20,7 +20,6 @@ import React from "react";
  * freely use on your site.
  */
 import image from "../images/design-desk.jpeg";
-
 const imageAltText = "desktop with books and laptop";
 
 /**
@@ -31,28 +30,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Auto RIA Clone (NestJS)",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed a clone of the popular Auto RIA platform using NestJS, showcasing skills in backend development and RESTful API design.",
+    url: "https://github.com/seemyoon/auto-ria-clone_nestjs",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Project The MovieDB",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created a movie database application utilizing APIs to display information about films, demonstrating proficiency in API integration and frontend development.",
+    url: "https://github.com/seemyoon/project_the_moviedb",
   },
 ];
 
